4346be0986e863fa435c2b703da2ca13006d28b5
MenuraDewalegama <61499065+MenuraDewalegama@users.noreply.github.com>
2020-10-10 15:25:28 +0530

    initial commit

---
+++ app/src/main/java/com/example/procurement/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.procurement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
